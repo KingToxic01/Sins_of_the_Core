2290dc4b08ee94f14012f962bb0b9626dc7f21eb
William <WilliamBatista01@outlook.com>
2023-03-23 15:46:48 -0400

    Initial commit

---
+++ Sonata/sonata.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+func _physics_process(delta):
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"), # <--- I added a comma
+		Input.get_action_strength("down") - Input.get_action_strength("up"))
+		
+	#print(input_direction) This print the direction of the characte movement in the vector
+	
