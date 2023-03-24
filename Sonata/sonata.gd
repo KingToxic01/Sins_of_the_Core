caafd4e2186010958b0187f9a9735a3e91e63cd3
KingToxic01 <Williambatista01@outlook.com>
2023-03-24 19:31:48 -0400

    Animation problem
    Still working on the animation

--- Sonata/sonata.gd
+++ Sonata/sonata.gd
@@ -1,9 +1,31 @@
 extends CharacterBody2D
+const MAX_SPEED = 80
+const ACCELERATION = 500
+const FRICTION = 500
+
+@onready var animation = $AnimationPlayer
+
+# Inputs for characters
+var left = Input.get_action_strength("left") 
+var right = Input.get_action_strength("right")
+var down = Input.get_action_strength("down")
+var up = Input.get_action_strength("up")
 
 func _physics_process(delta):
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"), # <--- I added a comma
-		Input.get_action_strength("down") - Input.get_action_strength("up"))
-		
-	print(input_direction) #This print the direction of the characte movement in the vector
+	velocity = Vector2.ZERO
+	var inpput_direction = Vector2.ZERO
+	inpput_direction.x = right - left
+	inpput_direction.y = down - up
+	inpput_direction =inpput_direction.normalized()
+	
+	if right:
+		animation.flip_h = false
+		animation.play("walkside")
+		velocity =  velocity.move_toward(inpput_direction * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animation.play("Idleside")
+		velocity =  velocity.move_toward(Vector2.ZERO,FRICTION * delta)
+	
+	
+	
 	
