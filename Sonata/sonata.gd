f7efd00ef2bd85a585b0443081a1cacba82fd191
MaxClears <123953642+MaxClears@users.noreply.github.com>
2023-05-09 20:06:24 -0400

    a

--- Sonata/sonata.gd
+++ Sonata/sonata.gd
@@ -1,18 +1,9 @@
 extends CharacterBody2D
 
-<<<<<<< Updated upstream
 var attack_cooldown = true
 var enemy_in_attack_range = false
 var speed = 80
 var attack_in_progress = false
-=======
-var enemy_inattack_range = false
-var enemy_attack_ticks = true
-var health = 100
-var sonata_alive = true
-var speed = 80
-var attack_ip = false
->>>>>>> Stashed changes
 var current_direct = "none"
 @onready var animation = $AnimationPlayer
 @onready var attack_deal = $Deal_attack_timer
@@ -31,17 +22,7 @@ func _ready():
 func _physics_process(delta):
 	EnemyAttack()
 	PlayerMovement(delta)
-<<<<<<< Updated upstream
 	Attack()
-=======
-	enemy_attack()
-	
-	if health <= 0:
-		sonata_alive = false # This is where you would add the END SCREEN IMPORTANT
-		health = 0
-		print("player has been disassembled")
-		self.queue_free()
->>>>>>> Stashed changes
 
 func PlayerMovement(delta):
 	if Input.is_action_pressed("right"):
@@ -77,17 +58,8 @@ func PlayerMovement(delta):
 		PlayAnimator(0)
 		velocity.x = 0
 		velocity.y = 0
-<<<<<<< Updated upstream
 	
 	
-=======
-
-#	if  Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-##		$player_hitbox/CollisionShape2D.disabled = false
-#		animation.play("frontAttack")
-#	else :
-##		$player_hitbox/CollisionShape2D.disabled = true
->>>>>>> Stashed changes
 	
 	move_and_slide()
 
@@ -99,43 +71,28 @@ func PlayAnimator(movement):
 		if movement == 1:
 			animation.play("walkside")
 		elif  movement == 0:
-<<<<<<< Updated upstream
 			if attack_in_progress == false:
 				animation.play("Idleside")
 			
-=======
-			if attack_ip == false:
-				animation.play("Idleside")
-	
->>>>>>> Stashed changes
 	if dir == "left":
 		if movement == 1:
 			animation.play("walkleft")
 		elif  movement == 0:
-<<<<<<< Updated upstream
 			if attack_in_progress == false:
-=======
-			if attack_ip == false:
->>>>>>> Stashed changes
 				animation.play("idleleft")
 			
 	if dir == "down":
 		if movement == 1:
 			animation.play("walkfront")
 		elif  movement == 0:
-<<<<<<< Updated upstream
 			if attack_in_progress == false:
-=======
-			if attack_ip == false:
->>>>>>> Stashed changes
 				animation.play("Idlefront")
 			
 	if dir == "up":
 		if movement == 1:
 			animation.play("walkback")
 		elif  movement == 0:
-			if attack_ip == false:
-				animation.play("idleback")
+			animation.play("idleback")
 		
 func _on_front_attack_body_entered(body):
 	if body.has_method("Enemy"):
@@ -242,59 +199,23 @@ func Player():
 #			animation.play("idleback")
 #	# Move the character
 #	move_and_slide()
-func player():
-	pass
+	
 
-func _on_player_hitbox_body_entered(body):
-	if body.has_method("enemy"):
-		enemy_inattack_range = true
+	
+	
+	
+	
 
-func _on_player_hitbox_body_exited(body):
-	if body.has_method("enemy"):
-		enemy_inattack_range = false
-
-func enemy_attack():
-	if enemy_inattack_range and enemy_attack_ticks == true:
-		health = health - 10
-		enemy_attack_ticks = false
-		$attack_cooldown.start()
-		print(health)
-
-func _on_attack_cooldown_timeout():
-	enemy_attack_ticks = true
-
-func attack():
-	var dir = current_direct
-	if Input.is_action_just_pressed("MOUSE_BUTTON_LEFT"):
-		global.player_current_attack = true
-		attack_ip = true
-		animation.play("frontAttack")
-		$deal_attack_timer.start()
-#		if dir == "right":
-#			$AnimationPlayer.flip_h = false
-#			$AnimationPlayer.play("frontAttack")
-#			$deal_attack_timer.start()
-#		if dir == "left":
-#			$AnimationPlayer.flip_h = true
-#			$AnimationPlayer.play("frontAttack")
-#			$deal_attack_timer.start()
-#		if dir == "down":
-#			$AnimationPlayer.play("frontAttack")
-#			$deal_attack_timer.start()
-#		if dir == "up":
-#			$AnimationPlayer.flip_v = true
-#			$AnimationPlayer.play("frontAttack")
-#			$deal_attack_timer.start()
-
-<<<<<<< Updated upstream
+	
+	
+	
 
 
 
 
 
-=======
-func _on_deal_attack_timer_timeout():
-	$deal_attack_timer.stop()
-	global.player_current_attack = false
-	attack_ip = false
->>>>>>> Stashed changes
+
+
+
+
+
